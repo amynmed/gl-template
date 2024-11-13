f1f8645203453c7e801b8719476bfdbeba9f85bf
medameen <medameen6@gmail.com>
2024-11-13 19:10:37 +0100

    -

--- main.cpp
+++ main.cpp
@@ -7,25 +7,39 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
+#define TITLE   "renderer"
 
-#define WIDTH  800
-#define HEIGHT 600
+#define WIDTH   800
+#define HEIGHT  600
 
 
 int main()
 {
-        std::cout << "renderer" << std::endl;
 
         std::chrono::high_resolution_clock clk;
+
+
+
+        glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
         if (!glfwInit())
         {
                 std::cout << "GLFW initialization error" << std::endl;
         }
 
-        glfwTerminate();
 
-        GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
+        GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, NULL, NULL);
+
+        if (!window) 
+        {
+                std::cerr << "Failed to create GLFW window" << std::endl;
+                glfwTerminate();
+                return -1;
+        }
+
         glfwMakeContextCurrent(window);
 
         int version = gladLoadGL(glfwGetProcAddress);
@@ -37,6 +51,26 @@ int main()
 
         // Successfully loaded OpenGL
         printf("Loaded OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+
+        while (!glfwWindowShouldClose(window))
+        {
+                // Render here (e.g., clear the screen)
+                glClear(GL_COLOR_BUFFER_BIT);
+
+                // Swap front and back buffers
+                glfwSwapBuffers(window);
+
+                // Poll for and process events
+                glfwPollEvents();
+        }
+
+        glfwTerminate();
         
+
+
+
+
+
+
         return 0;
 }
